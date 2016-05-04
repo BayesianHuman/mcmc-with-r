b4a73a63f8b106696bcfb076056eb5d4ccf4c659
BayesianHuman <tsibina.dasha@yandex.ru>
2016-05-04 22:04:08 +0400

    added normal distribution & CLT

--- ChapterTwo.R
+++ ChapterTwo.R
@@ -1,4 +1,7 @@
-# Chapter 2: Random Variable Generation (Book: Introducing Monte Carlo Methods with R)
+# CHAPTER 2: Random Variable Generation (Book: Introducing Monte Carlo Methods with R)
+
+
+#------------------------------- Inverse Transform -----------------------------------
 
 
 # Ex 1: Simple simulations from uniform distribution
@@ -27,6 +30,32 @@ Y    = rlogis(Nsim)
 par(mfrow = c(1,2))
 hist(X, freq = F, main = "Logistic from inverse transformation")
 hist(Y, freq = F, main = "Logistic distribution using rlogis()")
+
+
+#---------------------------------- Normal distribution & CLT ---------------------------------
+
+norm.clt <- function(){
+	# Generate approximation of normal distribution using
+	# Central Limit Theorem
+	U    = runif(12) - 0.5
+    Z    = sum(U)  #  E(Z) = 0  and  Var(Z) = 1
+    return (Z)
+}
+
+Nsim = 1000
+X    = replicate(Nsim,norm.clt(),simplify = "array")
+Y    = rnorm(Nsim)
+par(mfrow = c(1,2))
+hist(X, freq = F, main = "Normal distribution simulation using CLT")
+hist(Y, freq = F, main = "R function for normal distribution simulation") 
+
+
+#------------------------------------- Accept - Reject Method -------------------------------
+
+
+
+
+
 
 
 
